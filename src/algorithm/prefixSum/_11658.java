cad48edeee7889dc5129cba0129ac287ee7b5068
Damsul <gyoung9707@naver.com>
2023-01-06 20:56:27 +0900

    11658_구간 합 구하기3
    누적합, 시간초과

--- src/algorithm/prefixSum/_11658.java
+++ src/algorithm/prefixSum/_11658.java
@@ -7,49 +7,47 @@ import java.util.StringTokenizer;
 
 public class _11658 {
 
-    public static int n, m, w, x, y, c, x1, y1, x2, y2;
-
-    public static int[][] psum, a;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        psum = new int[n + 1][n + 1];
-        a = new int[n + 1][n + 1];
+        int[][] psum = new int[n + 1][n + 1];
+        int[][] a = new int[n + 1][n + 1];
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= n; j++) {
                 a[i][j] = Integer.parseInt(st.nextToken());
-                psum[i][j] = psum[i - 1][j] + psum[i][j - 1] - psum[i - 1][j - 1] + a[i][j];
+                psum[i][j] = psum[i][j - 1] + a[i][j];
             }
         }
 
         while (m-- > 0) {
             st = new StringTokenizer(br.readLine());
-            w = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
             if (w == 1) {
-                x1 = Integer.parseInt(st.nextToken());
-                y1 = Integer.parseInt(st.nextToken());
-                x2 = Integer.parseInt(st.nextToken());
-                y2 = Integer.parseInt(st.nextToken());
+                int x1 = Integer.parseInt(st.nextToken());
+                int y1 = Integer.parseInt(st.nextToken());
+                int x2 = Integer.parseInt(st.nextToken());
+                int y2 = Integer.parseInt(st.nextToken());
 
-                int sum = psum[x2][y2] - psum[x1 - 1][y2] - psum[x2][y1 - 1] + psum[x1 - 1][y1 - 1];
+                int sum = 0;
+                for (int i = x1; i <= x2; i++) {
+                    sum += psum[i][y2] - psum[i][y1 - 1];
+                }
                 System.out.println(sum);
             }
             if (w == 0) {
-                x = Integer.parseInt(st.nextToken());
-                y = Integer.parseInt(st.nextToken());
-                c = Integer.parseInt(st.nextToken());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                int c = Integer.parseInt(st.nextToken());
 
                 a[x][y] = c;
-                for (int i = x; i <= n; i++) {
-                    for (int j = y; j <= n; j++) {
-                        psum[i][j] = psum[i - 1][j] + psum[i][j - 1] - psum[i - 1][j - 1] + a[i][j];
-                    }
+                for (int i = y; i <= n; i++) {
+                    psum[x][i] = psum[x][i - 1] + a[x][i];
                 }
             }
         }
