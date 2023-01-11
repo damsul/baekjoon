a3bc1acc116f1e324e3a07dab6a5424438741650
Damsul <gyoung9707@naver.com>
2023-01-11 20:33:26 +0900

    [성공]1406_에디터
    링크드리스트
    if -> switch
    출력방식을 바꾸니 성공했다.
    
    처음 구현한 방법에서는 cursor 변수를 이용해 커서의 위치를 다루었고, L/D/B/P 시 LinkedList가 커서의 위치, 즉 해당하는 인덱스를 찾고 삽입/삭제가 처리되는 방식이었다.
    
    하지만 이처럼 위치를 찾아가지 말고 문제의 '커서'처럼 해당하는 위치에 계속 있으면서 삽입/삭제를 처리할 수 있게 하는 방법 ListIterator의 사용을 찾았다.
    
    Iterator의 단방향 탐색과 달리 양방향 탐색이 가능하다.
    그렇기에 이 문제처럼 양방향으로 이동하면서 수정하기에 효율적이다.

--- src/dataStructure/linkedlist/_1406.java
+++ src/dataStructure/linkedlist/_1406.java
@@ -3,76 +3,53 @@ package dataStructure.linkedlist;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.List;
+import java.util.ListIterator;
 
 public class _1406 {
 
-    public static LinkedList<String> list;
-    public static int cursor;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        LinkedList<String> list = new LinkedList<>();
         String str = br.readLine().toLowerCase(); // 문자열 입력
-        list = new LinkedList<>();
         for (String s : str.split("")) { // 연결리스트로 변환
             list.add(s);
         }
-        cursor = str.length(); // 커서 생성
 
         int n = Integer.parseInt(br.readLine()); //명령 횟수 입력
 
+        ListIterator<String> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            iterator.next();
+        }
+
         while (n-- > 0) {
             String[] temp = br.readLine().split(" "); // 명령어 담을 공간 생성 및 명령어 입력
-            if (temp[0].equals("L")) { // 명령어 판단
-                moveLeft(); // 명령어 실행
-            }
-            if (temp[0].equals("D")) {
-                moveRight();
-            }
-            if (temp[0].equals("B")) {
-                delete();
-            }
-            if (temp[0].equals("P")) {
-                String s = temp[1];
-                insert(s);
+            switch (temp[0]) {
+                case "L":  // 명령어 판단
+                    if (iterator.hasPrevious()) {
+                        iterator.previous(); // 명령어 실행
+                    }
+                    break;
+                case "D":
+                    if (iterator.hasNext()) {
+                        iterator.next();
+                    }
+                    break;
+                case "B":
+                    if (iterator.hasPrevious()) {
+                        iterator.previous();
+                        iterator.remove();
+                    }
+                    break;
+                case "P":
+                    String s = temp[1];
+                    iterator.add(s);
+                    break;
             }
         } // 명령어 종료
-        print(); //문자열 출력
-    }
 
-    private static void print() {
-        Iterator<String> iterator = list.iterator();
-        while (iterator.hasNext()) {
-            System.out.print(iterator.next());
-        }
-    }
+        System.out.println(String.join("", list)); // 문자 이어붙이기
 
-    private static void insert(String s) {
-        list.add(cursor, s);
-        cursor++;
-    }
-
-    private static void delete() {
-        if (cursor <= 0) {
-            return;
-        }
-        list.remove(cursor - 1);
-        cursor--;
-    }
-
-    private static void moveRight() {
-        if (cursor >= list.size()) {
-            return;
-        }
-        cursor++;
-    }
-
-    private static void moveLeft() {
-        if (cursor <= 0) {
-            return;
-        }
-        cursor--;
     }
 }
