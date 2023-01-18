f8cce8beedaebf3be0b49359e3251d4cfedfb272
Damsul <gyoung9707@naver.com>
2023-01-18 18:03:08 +0900

    [성공]10828_스택
    스택-연습문제
    LinkedList로 Stack 구현

--- src/dataStructure/stack/_10828.java
+++ src/dataStructure/stack/_10828.java
@@ -3,48 +3,77 @@ package dataStructure.stack;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.LinkedList;
 
 public class _10828 {
+    public static LinkedList<Integer> stack;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        Stack<Integer> stack = new Stack<>();
+        int n = Integer.parseInt(br.readLine());
+        stack = new LinkedList<>();
 
         while (n-- > 0) {
             String[] arr = br.readLine().split(" ");
             String command = arr[0];
+
             switch (command) {
                 case "push":
-                    stack.push(Integer.parseInt(arr[1]));
+                    int x = Integer.parseInt(arr[1]);
+                    push(x);
                     break;
                 case "pop":
-                    if (stack.isEmpty()) {
-                        System.out.println(-1);
-                    } else {
-                        System.out.println(stack.pop());
-                    }
+//                    System.out.println(pop());
+                    sb.append(pop()).append("\n");
                     break;
                 case "size":
-                    System.out.println(stack.size());
+//                    System.out.println(size());
+                    sb.append(size()).append("\n");
                     break;
                 case "empty":
-                    if (stack.isEmpty()) {
-                        System.out.println(1);
-                    } else {
-                        System.out.println(0);
-                    }
+//                    System.out.println(empty());
+                    sb.append(empty()).append("\n");
                     break;
                 case "top":
-                    if (stack.isEmpty()) {
-                        System.out.println(-1);
-                    } else {
-                        System.out.println(stack.peek());
-                    }
+//                    System.out.println(top());
+                    sb.append(top()).append("\n");
                     break;
             }
+        }
+        System.out.println(sb);
+    }
+
+    private static void push(int x) {
+        stack.addLast(x);
+    }
+
+    private static int pop() {
+        if (stack.isEmpty()) {
+            return -1;
+        } else {
+            return stack.removeLast();
+        }
+    }
+
+    private static int size() {
+        return stack.size();
+    }
+
+    private static int empty() {
+        if (stack.isEmpty()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    private static int top() {
+        if (stack.isEmpty()) {
+            return -1;
+        } else {
+            return stack.getLast();
         }
     }
 
