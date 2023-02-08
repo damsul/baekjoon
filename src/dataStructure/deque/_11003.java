fe4465da2175a08deb4679b6c73f2ea6c235cfd8
Damsul <gyoung9707@naver.com>
2023-02-08 17:07:39 +0900

    [성공]11003_최솟값 찾기
    배열의 인덱스를 덱에 담아서 사용함

--- src/dataStructure/deque/_11003.java
+++ src/dataStructure/deque/_11003.java
@@ -3,7 +3,6 @@ package dataStructure.deque;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
 import java.util.LinkedList;
 
 public class _11003 {
@@ -15,46 +14,49 @@ public class _11003 {
         int L = Integer.parseInt(str1[1]);
 
         String[] data = br.readLine().split(" ");
-        LinkedList<Integer> A = new LinkedList<>(); // data
-        LinkedList<Integer> D = new LinkedList<>(); // i-L+1 ~ i 중의 최솟값
-
-        /**
-         * 0 = 0-3+1(-2) ~ 0    0
-         * 1 = 1-3+1(-1) ~ 1    01
-         * 2 = 2-3+1(+0) ~ 2    012
-         * 3 = 3-3+1(+1) ~ 3    123
-         * 4 = 4-3+1(+2) ~ 4    234
-         * 5 = 5-3+1(+3) ~ 5    345
-         * 6 = 6-3+1(+4) ~ 6    456
-         * 7 = 7-3+1(+5) ~ 7    567
-         * 8 = 8-3+1(+6) ~ 8    678
-         * 9 = 9-3+1(+7) ~ 9    789
-         * 10 = 10-3+1(+8) ~ 10
-         * 11 = 11-3+1(+9) ~ 11
-         * 12 = 12-3+1(+10) ~ 12
-         */
+        int[]A = new int[N]; // data
+        int[]D = new int[N]; // i-L+1 ~ i 중의 최솟값
 
         for (int i = 0; i < N; i++) {
-            A.addLast(Integer.parseInt(data[i]));
-            int min = Integer.MAX_VALUE;
+            A[i] = Integer.parseInt(data[i]);
+        }
 
-            for (int j = i - L + 1; j <= i; j++) {
-                if (j < 0) {
-                    continue;
-                }
-                if (Integer.parseInt(data[j]) < min) {
-                    min = Integer.parseInt(data[j]);
-                }
+        LinkedList<Integer> deque = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
+
+            while (!deque.isEmpty() && A[deque.getLast()] > A[i]) {
+                deque.pollLast();
             }
+            while (!deque.isEmpty() && deque.getFirst() <= i - L) {
+                deque.pollFirst();
+            }
+            deque.addLast(i);
 
-            D.addLast(min);
+            D[i] = A[deque.getFirst()];
         }
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
-            sb.append(D.pollFirst()).append(" ");
+            sb.append(D[i]).append(" ");
         }
         System.out.println(sb);
+        /*
+          0 = 0-3+1(-2) ~ 0    0
+          1 = 1-3+1(-1) ~ 1    01
+          2 = 2-3+1(+0) ~ 2    012
+          3 = 3-3+1(+1) ~ 3    123
+          4 = 4-3+1(+2) ~ 4    234
+          5 = 5-3+1(+3) ~ 5    345
+          6 = 6-3+1(+4) ~ 6    456
+          7 = 7-3+1(+5) ~ 7    567
+          8 = 8-3+1(+6) ~ 8    678
+          9 = 9-3+1(+7) ~ 9    789
+          10 = 10-3+1(+8) ~ 10
+          11 = 11-3+1(+9) ~ 11
+          12 = 12-3+1(+10) ~ 12
+         */
+
+
     }
 
 }
