8d17af6f77efad1fca2135aa0a911a8bc47aa4b6
Damsul <gyoung9707@naver.com>
2023-02-20 20:16:28 +0900

    [성공]10814_나이순 정렬
    정렬
    
    2차 배열 -> Member class 만들고 1차 배열로 구현

--- src/algorithm/sort1/_10814.java
+++ src/algorithm/sort1/_10814.java
@@ -7,8 +7,8 @@ import java.util.StringTokenizer;
 
 public class _10814 {
     public static int N;
-    public static String[][] board = new String[100001][2];
-    public static String[][] tmp = new String[100001][2];
+    public static Member[] members = new Member[100001];
+    public static Member[] tmp = new Member[100001];
 
     public static void merge(int st, int en) {
         int mid = (st + en) / 2;
@@ -16,14 +16,14 @@ public class _10814 {
         int right = mid;
 
         for (int i = st; i < en; i++) {
-            if (right == en) tmp[i] = board[left++];
-            else if (left == mid) tmp[i] = board[right++];
-            else if (Integer.parseInt(board[left][0]) <= Integer.parseInt(board[right][0]))
-                tmp[i] = board[left++];
-            else tmp[i] = board[right++];
+            if (right == en) tmp[i] = members[left++];
+            else if (left == mid) tmp[i] = members[right++];
+            else if (members[left].age <= members[right].age)
+                tmp[i] = members[left++];
+            else tmp[i] = members[right++];
         }
         for (int i = st; i < en; i++) {
-            board[i] = tmp[i];
+            members[i] = tmp[i];
         }
     }
     public static void mergeSort(int st, int en) {
@@ -41,19 +41,30 @@ public class _10814 {
         StringTokenizer st;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            String age = st.nextToken();
+            int age = Integer.parseInt(st.nextToken());
             String name = st.nextToken();
-            board[i][0] = age;
-            board[i][1] = name;
+//            members[i].age = age;
+//            members[i].name = name;
+            members[i] = new Member(age, name);
         }
 
         mergeSort(0, N);
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
-            sb.append(board[i][0]).append(' ').append(board[i][1]).append('\n');
+            sb.append(members[i].age).append(' ').append(members[i].name).append('\n');
         }
         System.out.println(sb);
 
+    }
+
+    public static class Member {
+        private final int age;
+        private final String name;
+
+        public Member(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
     }
 }
