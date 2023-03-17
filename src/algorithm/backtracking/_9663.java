13f5acfa6b2b15bdc0c58775c3bc0945e640f18a
Damsul <gyoung9707@naver.com>
2023-03-17 12:08:10 +0900

    [Backtracking]9663_N-Queen

--- src/algorithm/backtracking/_9663.java
+++ src/algorithm/backtracking/_9663.java
@@ -5,37 +5,38 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class _9663 {
-    public static int N, cnt;
-    public static boolean[] issued1 = new boolean[30]; // y
-    public static boolean[] issued2 = new boolean[30]; // x+y
-    public static boolean[] issued3 = new boolean[30]; // x-y+n-1
 
-    public static void solve(int cur) {
-        if (cur == N) {
+    public static int n, cnt;
+    public static boolean[] visit1 = new boolean[30]; // 세로
+    public static boolean[] visit2 = new boolean[30]; // 오른쪽 위 대각선
+    public static boolean[] visit3 = new boolean[30]; // 오른쪽 아래 대각선
+
+    public static void func(int cur) {
+        if (cur == n){
             cnt++;
             return;
         }
-        for (int i = 0; i < N; i++) {
-            if(issued1[i]) continue;
-            if(issued2[cur + i]) continue;
-            if(issued3[cur - i + N - 1]) continue;
 
-            issued1[i] = true;
-            issued2[cur + i] = true;
-            issued3[cur - i + N - 1] = true;
-            solve(cur + 1);
 
-            issued1[i] = false;
-            issued2[cur + i] = false;
-            issued3[cur - i + N - 1] = false;
+        for (int i = 0; i < n; i++) {
+            if (visit1[i] || visit2[cur + i] || visit3[cur - i + n - 1]) continue;
+            visit1[i] = true;
+            visit2[cur + i] = true;
+            visit3[cur - i + n - 1] = true;
+
+            func(cur + 1);
+
+            visit1[i] = false;
+            visit2[cur + i] = false;
+            visit3[cur - i + n - 1] = false;
         }
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
 
-        solve(0);
+        func(0);
+
         System.out.println(cnt);
     }
-
 }
